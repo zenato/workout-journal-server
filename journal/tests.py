be08bbc9c78fed616e023af6fcb8aced2cd5e891
Yeongjin Lee <yeongjinnn@gmail.com>
2017-09-21 15:02:58 +0900

    Add BaseTestCase class.

--- journal/tests.py
+++ journal/tests.py
@@ -1,38 +1,52 @@
 import json
-from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework.test import APIRequestFactory, force_authenticate
 from .models import User
 from . import views
 
+TEST_USERNAME = 'zenato'
 
-class EventTests(APITestCase):
+
+class BaseTestCase(APITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(BaseTestCase, cls).setUpClass()
+        cls.factory = APIRequestFactory()
+        cls.user = User.objects.get(username=TEST_USERNAME)
+
+    def query(self, request, view):
+        force_authenticate(request, user=self.user)
+        response = view(request)
+        response.render()
+        return json.loads(response.content)
+
+
+class EventTests(BaseTestCase):
+    """
+    Ensure we can create a new event object.
+    """
+
     def test_create_event(self):
-        """
-        Ensure we can create a new event object.
-        """
-        url = reverse('journal-events')
-        data = {
+        request = self.factory.post('/events', {
             'name': 'name',
             'unit': 'unit',
             'value': 10,
             'remark': 'remark',
-        }
+        })
 
-        factory = APIRequestFactory()
-        request = factory.post(url, data)
-
-        user = User.objects.get(username='zenato')
-        force_authenticate(request, user=user)
-
-        view = views.EventList.as_view()
-        response = view(request)
-        response.render()
-
-        result = json.loads(response.content)
+        result = self.query(request, views.EventList.as_view())
 
         self.assertEqual(result['name'], 'name')
         self.assertEqual(result['unit'], 'unit')
         self.assertEqual(result['value'], 10)
         self.assertEqual(result['remark'], 'remark')
 
+    """
+    Ensure we can get event objects.
+    """
+
+    def test_list_event(self):
+        request = self.factory.get('/events')
+        result = self.query(request, views.EventList.as_view())
+
+        self.assertTrue(len(result) > 0)
