d0e0bb1dae6c7bc084e6f114c1d6e52ab17cf7ba
Yeongjin Lee <yeongjinnn@gmail.com>
2017-09-25 10:32:22 +0900

    Add resolve an object.

--- journal/schema.py
+++ journal/schema.py
@@ -21,26 +21,25 @@ class PerformanceNode(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_events = graphene.List(EventNode, name=graphene.String())
+    event = graphene.Field(EventNode, id=graphene.ID(required=True))
+
     all_posts = graphene.List(PostNode, name=graphene.String())
+    post = graphene.Field(PostNode, id=graphene.ID(required=True))
 
     def resolve_all_events(self, info, name=None):
-        where = {
-            'owner': info.context.user,
-        }
-
+        where = {'owner': info.context.user, }
         if name:
             where['name__contains'] = name
+        return Event.objects.filter(**where)
 
-        items = Event.objects.filter(**where)
-        return items
+    def resolve_event(self, info, id=None):
+        return Event.objects.get(owner=info.context.user, pk=id)
 
     def resolve_all_posts(self, info, name=None):
-        where = {
-            'owner': info.context.user,
-        }
-
+        where = {'owner': info.context.user, }
         if name:
             where['performances__event__name'] = name
+        return Post.objects.filter(**where)
 
-        items = Post.objects.filter(**where)
-        return items
+    def resolve_post(self, info, id=None):
+        return Post.objects.get(owner=info.context.user, pk=id)
--- journal/tests.py
+++ journal/tests.py
@@ -1,11 +1,9 @@
 import graphene
 from django.test import TestCase
 from graphene.test import Client
-from .models import User
+from .models import User, Event, Post, Performance
 from .schema import Query
 from .mutation import Mutation
-
-TEST_USERNAME = 'zenato'
 
 
 class MockContext(object):
@@ -26,30 +24,118 @@ class BaseTestCase(TestCase):
     def setUpClass(cls):
         super(BaseTestCase, cls).setUpClass()
 
-        cls.user = User.objects.get(username=TEST_USERNAME)
+        cls.user = User.objects.get(pk=1)
         cls.schema = graphene.Schema(query=TestQuery, mutation=TestMutation)
 
 
+# Event
+
 class EventTests(BaseTestCase):
+    def create(self):
+        event = Event(
+            name='test',
+            unit='kg',
+            value=10,
+            remark='test-remark',
+            owner=self.user,
+        )
+        event.save()
+        self.event = event
+
     """
     Ensure we can get a event objects.
     """
+
     def test_query_events(self):
+        self.create()
         client = Client(self.schema)
         executed = client.execute(
             '''
-                query {
-                    allEvents(name: "test") { name }
-                }
+            query {
+                allEvents(name: "test") { name }
+            }
             ''',
             context_value=MockContext(self.user),
         )
-        self.assertIsNotNone(executed.get('data'))
+        self.assertTrue(len(executed.get('data')) > 0)
+
+    """
+    Ensure we can get a event object.
+    """
+
+    def test_query_event(self):
+        self.create()
+        client = Client(self.schema)
+        executed = client.execute(
+            '''
+            query {
+                event(id: %s) { name }
+            }
+            ''' % self.event.id,
+            context_value=MockContext(self.user),
+        )
+        event = executed.get('data').get('event')
+        self.assertIsNotNone(event)
+
+    """
+    Ensure we can create event
+    """
+
+    def test_create_event(self):
+        client = Client(self.schema)
+        executed = client.execute(
+            '''
+            mutation {
+              createEvent(data: {name: "test", unit: "KG", value: 10, remark: "test-remark"}) {
+                event {
+                  id
+                }
+              }
+            }
+            ''',
+            context_value=MockContext(self.user),
+        )
+        event = executed.get("data").get('createEvent').get("event")
+        self.assertIsNotNone(event)
+
+
+# Post
+
+class PostTests(BaseTestCase):
+    def create(self):
+        event = Event(
+            name='test',
+            unit='kg',
+            value=10,
+            remark='test-remark',
+            owner=self.user,
+        )
+        event.save()
+
+        post = Post(
+            owner=self.user,
+        )
+        post.save()
+
+        Performance(
+            post=post,
+            event=event,
+            value=10,
+            set1=1,
+            set2=2,
+            set3=3,
+            set4=4,
+            set5=5,
+        ).save()
+
+        self.post = post
 
     """
     Ensure we can get a post objects.
     """
+
     def test_query_posts(self):
+        self.create()
         client = Client(self.schema)
         executed = client.execute(
             '''
@@ -66,4 +152,63 @@ class EventTests(BaseTestCase):
             ''',
             context_value=MockContext(self.user),
         )
-        self.assertIsNotNone(executed.get('data'))
+        self.assertTrue(len(executed.get('data')) > 0)
+
+    """
+    Ensure we can get a post object.
+    """
+
+    def test_query_post(self):
+        self.create()
+        client = Client(self.schema)
+        executed = client.execute(
+            '''
+                query {
+                    post(id: %s) {
+                        workoutDate
+                        performances {
+                            id
+                            event {
+                                name
+                            }
+                        }
+                    }
+                }
+            ''' % self.post.id,
+            context_value=MockContext(self.user),
+        )
+        post = executed.get('data').get('post')
+        self.assertIsNotNone(post)
+
+    """
+    Ensure we can create event
+    """
+
+    def test_create_post(self):
+        client = Client(self.schema)
+        executed = client.execute(
+            '''
+            mutation {
+              createPost(
+                data: { workoutDate: "2017-01-10T21:33:15.233Z", remark: "latest" },
+                performances: [{ event: 17, value: 20, set1: 10 }, { event: 48, value: 10, set1: 20 }]
+              ) {
+                post {
+                  id
+                  workoutDate
+                  remark
+                  performances {
+                    event {
+                      name
+                    }
+                    value
+                    set1
+                  }
+                }
+              }
+            }
+            ''',
+            context_value=MockContext(self.user),
+        )
+        post = executed.get("data").get('createPost').get("post")
+        self.assertIsNotNone(post)
